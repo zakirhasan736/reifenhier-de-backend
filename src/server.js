38679d1c53a6f3aa03f679d00e87b060224efe33
zakirhossaib736 <zakirhossaib736@gmail.com>
2025-06-26 12:37:45 +0600

    updated backend code

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 // src/server.js
 import app from './app.js';
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
 app.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
