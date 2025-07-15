1befc69a4de5432da470a35843c359fe2ba810fa
zakirhossaib736 <zakirhossaib736@gmail.com>
2025-07-15 19:13:36 +0600

    updated

--- src/app.js
+++ src/app.js
@@ -32,7 +32,8 @@ const app = express();
 const allowedOrigin = process.env.FRONTEND_URL || 'http://localhost:3000';
 app.use(cookieParser());
 app.use(cors({
-    origin: allowedOrigin,
+    origin: "*",
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
     credentials: true,
 }));
 app.use(express.json());
